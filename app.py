0c111e441661f6a0196bd0ab08bad435a5224264
kurniawan194 <kurniawansyahputra164@gmail.com>
2025-07-23 01:06:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json
 
 # Load data
-with open('scrapy_project/data/books.json') as f:
+with open('data/books.json') as f:
     books = json.load(f)
 
 st.title('Books to Scrape')
